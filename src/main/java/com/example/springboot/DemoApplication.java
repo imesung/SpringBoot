aa08066157474c2e930ac96736b275274ea804d5
myhs2002 <myhs2002@gmail.com>
2019-10-23 20:18:31 +0900

    1주차 커밋 완료

--- src/main/java/com/example/springboot/DemoApplication.java
+++ src/main/java/com/example/springboot/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import autoConf.Holoman;
+
+//@SpringBootConfiguration
+//@ComponentScan
+//@EnableAutoConfiguration
 @SpringBootApplication
 public class DemoApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication application = new SpringApplication(DemoApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
+
 
 }
